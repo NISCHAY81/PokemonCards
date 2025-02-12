cdbe1b6c3c39eb9f8add994e702f07bbef97d5cf
NISCHAY81 <nischaykumar81@gmail.com>
2025-02-12 20:37:33 +0530

    first commit

---
+++ src/Pokemon.jsx
@@ -0,0 +1,83 @@
+import React, { useEffect, useState } from 'react'
+import "./index.css"
+import PokemonCards from './PokemonCards';
+const Pokemon = () => {
+  const [pokemon, setPokemon] = useState([]);
+  const[Loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [search, setSearch] = useState("");
+
+
+const API = "https://pokeapi.co/api/v2/pokemon?limit=124";
+
+const fetchPokemon = async() =>{
+ try {
+  const res = await fetch (API);
+  const data = await res.json();
+  console.log(data);
+
+  const detailedPokemonData = data.results.map(async(currPokemon) =>{
+    const res = await fetch(currPokemon.url)
+    const data = await res.json();
+   return data;
+  });
+  // console.log(detailsResponse);
+
+  const detailsResponse = await Promise.all(detailedPokemonData);
+  setPokemon(detailsResponse);
+  setLoading(false)
+  
+ } catch (error) {
+   console.log(error);
+   setLoading(false)
+   setError(error)
+ }
+};
+
+useEffect (() => {
+  fetchPokemon();
+},[]);
+ 
+//searchFunctionality
+
+const searchData = pokemon.filter((currPokemon) => currPokemon.name.toLowerCase().includes(search.toLowerCase()));
+
+if(Loading)
+  return(
+   <div>
+    <h1>Loading.....</h1>
+   </div>
+  )
+  
+  if(error)
+    return(
+    <div>
+      <h1>{error.message}</h1>
+    </div>
+    )
+
+  return (
+   <section className='container'>
+    <header>
+    <h1>Let's Catch Pokemon</h1>
+    </header>
+    <div className='pokemon-search'>
+     <input type="text"
+     placeholder='text'
+    value={search}
+    onChange={(e) => setSearch(e.target.value)}
+     />
+    </div>
+    <div>
+      <ul className='cards'>
+        {searchData.map((currPokemon) =>(
+         <PokemonCards key ={currPokemon.id} pokemonData = {currPokemon}/>
+        ))}
+      </ul>
+    </div>
+
+   </section>
+  )
+}
+
+export default Pokemon
